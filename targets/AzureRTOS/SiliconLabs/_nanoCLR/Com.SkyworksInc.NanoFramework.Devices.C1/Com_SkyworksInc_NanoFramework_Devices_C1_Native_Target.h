b0944c302fe7cb8aceb296c5e791e0ea89ccaed6
Carlos Garcia <carlos.garcia@skyworksinc.com>
2025-03-11 11:47:37 -0500

    Modified GPIO_SIGNAL macro to work off of configurable pins

--- targets/AzureRTOS/SiliconLabs/_nanoCLR/Com.SkyworksInc.NanoFramework.Devices.C1/Com_SkyworksInc_NanoFramework_Devices_C1_Native_Target.h
+++ targets/AzureRTOS/SiliconLabs/_nanoCLR/Com.SkyworksInc.NanoFramework.Devices.C1/Com_SkyworksInc_NanoFramework_Devices_C1_Native_Target.h
@@ -13,45 +13,18 @@
 #if defined(_SILICON_LABS_32B_SERIES_1) && (GECKO_USE_C1 == TRUE)
     #define REGISTER_BIT        (1 << C1_GPIO_PIN)
     #define GPIO_READ()         ((GPIO->P[C1_GPIO_PORT].DIN & REGISTER_BIT) ? 1 : 0)
-
-    // GPIO_SIGNAL(0) = LOW, GPIO_SIGNAL(1) = HIGH
-    //#define GPIO_SIGNAL(value)  (value ? (GPIO->P[C1_GPIO_PORT].DOUT |= REGISTER_BIT) : (GPIO->P[C1_GPIO_PORT].DOUT &= ~REGISTER_BIT))
-    
-    #define GPIO_SIGNAL_BK(state) \
-    do { \
-        if ((state) == 0) { \
-            GPIO->P[1].DOUT &= ~(1 << 9); \
-            GPIO->P[1].MODEL = (GPIO->P[1].MODEL & ~(_GPIO_P_MODEL_MODE0_MASK << (9 % 8 * 4))) | (GPIO_P_MODEL_MODE0_PUSHPULL << (9 % 8 * 4)); \
-        } else if ((state) == 1) { \
-            GPIO->P[1].DOUT |= (1 << 9); \
-            GPIO->P[1].MODEL = (GPIO->P[1].MODEL & ~(_GPIO_P_MODEL_MODE0_MASK << (9 % 8 * 4))) | (GPIO_P_MODEL_MODE0_PUSHPULL << (9 % 8 * 4)); \
-        } else if ((state) == 2) { \
-            GPIO->P[1].MODEL = (GPIO->P[1].MODEL & ~(_GPIO_P_MODEL_MODE0_MASK << (9 % 8 * 4))) | (GPIO_P_MODEL_MODE0_INPUT << (9 % 8 * 4)); \
-        } \
-    } while (0)
-
     #define GPIO_SIGNAL(state) \
     do { \
         if ((state) == 0) { \
-            GPIO->P[1].DOUT &= ~(1 << 9); \
-            GPIO->P[1].MODEH = (GPIO->P[1].MODEH & ~(_GPIO_P_MODEH_MODE8_MASK << (9 % 8 * 4))) | (GPIO_P_MODEH_MODE8_PUSHPULL << (9 % 8 * 4)); \
+            GPIO->P[C1_GPIO_PORT].DOUT &= ~(1 << C1_GPIO_PIN); \
+            GPIO->P[C1_GPIO_PORT].MODEH = (GPIO->P[C1_GPIO_PORT].MODEH & ~(_GPIO_P_MODEH_MODE8_MASK << (C1_GPIO_PIN % 8 * 4))) | (GPIO_P_MODEH_MODE8_PUSHPULL << (C1_GPIO_PIN % 8 * 4)); \
         } else if ((state) == 1) { \
-            GPIO->P[1].DOUT |= (1 << 9); \
-            GPIO->P[1].MODEH = (GPIO->P[1].MODEH & ~(_GPIO_P_MODEH_MODE8_MASK << (9 % 8 * 4))) | (GPIO_P_MODEH_MODE8_PUSHPULL << (9 % 8 * 4)); \
+            GPIO->P[C1_GPIO_PORT].DOUT |= (1 << C1_GPIO_PIN); \
+            GPIO->P[C1_GPIO_PORT].MODEH = (GPIO->P[C1_GPIO_PORT].MODEH & ~(_GPIO_P_MODEH_MODE8_MASK << (C1_GPIO_PIN % 8 * 4))) | (GPIO_P_MODEH_MODE8_PUSHPULL << (C1_GPIO_PIN % 8 * 4)); \
         } else if ((state) == 2) { \
-            GPIO->P[1].MODEH = (GPIO->P[1].MODEH & ~(_GPIO_P_MODEH_MODE8_MASK << (9 % 8 * 4))) | (GPIO_P_MODEH_MODE8_INPUT << (9 % 8 * 4)); \
+            GPIO->P[C1_GPIO_PORT].MODEH = (GPIO->P[C1_GPIO_PIN].MODEH & ~(_GPIO_P_MODEH_MODE8_MASK << (C1_GPIO_PIN % 8 * 4))) | (GPIO_P_MODEH_MODE8_INPUT << (C1_GPIO_PIN % 8 * 4)); \
         } \
     } while (0)
-
-    // Set the prescaler of timer 0 in order to increase or decrease the frequency
-    #define TIMER0_SET_PRESCALER(prescaler) {   \
-        TIMER0->CTRL = (TIMER0->CTRL & ~_TIMER_CTRL_PRESC_MASK) | prescaler;   \
-        TIMER0->CNT = 0;    \
-        TIMER0->CMD = TIMER_CMD_START;  \
-        TIMER0->IFC = TIMER_IFC_OF;     \
-        TIMER0->IEN = TIMER_IEN_OF;     \
-        NVIC->ISER[(((uint32_t)TIMER0_IRQn) >> 5)] = (1 << (((uint32_t)TIMER0_IRQn) & 0x1F));    \
-    }
 #else
 #error                                                                                                                 \
     "Only _SILICON_LABS_32B_SERIES_1 is supported at this time. To add support for other series declaration above has to be updated"
